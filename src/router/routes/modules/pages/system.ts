0ff8e412841fa254774607dfbc94e1d3f5043682
cj970116 <45820174+cj970116@users.noreply.github.com>
2023-02-06 14:37:41 +0800

    feat: 新增Form，Table，Upload组件

---
+++ src/router/routes/modules/pages/system.ts
@@ -0,0 +1,29 @@
+import type { AppRouteModule } from '/@/router/types'
+
+import { LAYOUT } from '/@/router/constant'
+import { t } from '/@/hooks/web/useI18n'
+
+const system: AppRouteModule = {
+  path: '/system',
+  name: 'System',
+  component: LAYOUT,
+  redirect: '/system/account',
+  meta: {
+    orderNo: 2000,
+    icon: 'ion:settings-outline',
+    title: t('routes.demo.system.moduleName'),
+  },
+  children: [
+    {
+      path: 'account',
+      name: 'AccountManagement',
+      meta: {
+        title: t('routes.demo.system.account'),
+        ignoreKeepAlive: false,
+      },
+      component: () => import('/@/views/pages/system/user/Index.vue'),
+    },
+  ],
+}
+
+export default system
